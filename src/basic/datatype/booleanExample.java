00ff0a9f8033b88bfce6f9f0684314bc304a24f4
wonjinseop <wonjinseop7@naver.com>
2024-02-20 17:19:24 +0900

    # Java 기초 문법(입/출력, datatype)

---
+++ src/basic/datatype/booleanExample.java
@@ -0,0 +1,18 @@
+package basic.datatype;
+
+public class booleanExample {
+
+    public static void main(String[] args) {
+
+        boolean b1 = true;
+        boolean b2 = false;
+
+//        boolean d3 = True; (x)
+//        boolean b4 = False; (x)
+//        boolean b5 = 0; (x)
+//        boolean b6 = "논리형 타입"; (x)
+
+
+    }
+
+}
