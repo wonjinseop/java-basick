8716c0a0088e56b8fea45921253df7849cbd5053
wonjinseop <wonjinseop7@naver.com>
2024-02-21 10:37:27 +0900

    # Java 기초 문법(연산자)

---
+++ src/basic/operator/UnaryOperator.java
@@ -0,0 +1,31 @@
+package basic.operator;
+
+public class UnaryOperator {
+
+    public static void main(String[] args) {
+
+        // 증감 연산자 (++, --)
+        // 변수의 값을 단순히 하나 올리거나 내릴 때 사용합니다.
+
+        int i = 1;
+        int j = i++;
+        int k = i--;
+
+        System.out.println("i = " + i);
+        System.out.println("j = " + j);
+        System.out.println("k = " + k);
+
+        System.out.println("-------------------------------------------");
+
+        int x = 1;
+        int y = ++x; // 전위 연산 (선 증감 후 연산)
+        int z = --y;
+
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+        System.out.println("z = " + z);
+
+
+    }
+
+}
