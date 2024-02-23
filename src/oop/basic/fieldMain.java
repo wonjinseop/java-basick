06fbd97155a4463d034c17f67b1530a173db1999
wonjinseop <wonjinseop7@naver.com>
2024-02-23 12:23:53 +0900

    # Java oop(필드와 지역변수, String)

---
+++ src/oop/basic/fieldMain.java
@@ -0,0 +1,14 @@
+package oop.basic;
+
+public class fieldMain {
+
+    public static void main(String[] args) {
+
+        FieldAndLocal fl = new FieldAndLocal();
+        fl.a = 2;
+        fl.printNumber(4);
+        fl.foo();
+
+    }
+
+}
