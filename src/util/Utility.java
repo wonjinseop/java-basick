33b79e3e10180c747c06cfe8abaf27379e82267d
wonjinseop <wonjinseop7@naver.com>
2024-03-06 17:14:38 +0900

    # Java Basic(음악 관리 프로그램 작성)

--- src/util/Utility.java
+++ src/util/Utility.java
@@ -4,7 +4,13 @@ import java.util.Scanner;
 
 public class Utility {
     
-    static Scanner sc = new Scanner(System.in);
+    public static final String FILE_PATH = "C:/myWorkSpace/music";
+    
+    static Scanner sc;
+    
+    static {
+        sc = new Scanner(System.in);
+    }
     
     // 입력창의 구분을 위해 줄을 그어주는 메서드
     public static void makeLine() {
@@ -13,7 +19,7 @@ public class Utility {
     
     public static String input(String message) {
         System.out.print(message);
-        return sc.next();
+        return sc.nextLine();
     }
     
 }
